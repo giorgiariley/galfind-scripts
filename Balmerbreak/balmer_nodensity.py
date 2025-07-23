f4518f759ee3f8f5ae8fb65043b1008862a621ce
Giorgia Riley Summer 2025 <griley@morgan.jb.man.ac.uk>
2025-07-23 10:58:37 +0100

    Added in Austin+25 sample, now works using bagpipes SEDs not EAZY

--- Balmerbreak/balmer_nodensity.py
+++ Balmerbreak/balmer_nodensity.py
@@ -3,25 +3,63 @@ from typing import Union
 import os
 os.environ['GALFIND_CONFIG_NAME'] = 'galfind_config_Griley.ini'
 from galfind.Data import morgan_version_to_dir
-from galfind import Catalogue, EAZY, Bagpipes, Redshift_Extractor
+from galfind import Catalogue, EAZY, Bagpipes, Redshift_Extractor, SED_code
 import matplotlib.pyplot as plt
 import numpy as np
 from astropy.cosmology import FlatLambdaCDM
 from astropy.io import fits
 from astropy.table import Table 
 from scipy.stats import norm
-from Balmerbreak import Austin25_sample
+from galfind import galfind_logger, Multiple_Mask_Selector, Multiple_SED_fit_Selector, Min_Instrument_Unmasked_Band_Selector, Unmasked_Band_Selector, Bluewards_LyLim_Non_Detect_Selector, Bluewards_Lya_Non_Detect_Selector, Redwards_Lya_Detect_Selector, Chi_Sq_Lim_Selector, Chi_Sq_Diff_Selector, Robust_zPDF_Selector, Sextractor_Bands_Radius_Selector    
 from scipy.stats import ks_2samp
 from scipy.interpolate import interp1d
 
 
+#selecting the sample that consists of (hopefully) only galaxies and gets rid of any nonsense!
+class Austin25_unmasked_criteria(Multiple_Mask_Selector):
 
+    def __init__(self):
+        selectors = [
+            Min_Instrument_Unmasked_Band_Selector(min_bands = 2, instrument = "ACS_WFC"),
+            Min_Instrument_Unmasked_Band_Selector(min_bands = 6, instrument = "NIRCam"),
+        ]
+        selectors.extend([Unmasked_Band_Selector(band) for band in ["F090W", "F277W", "F356W", "F410M", "F444W"]])
+        selection_name = "Austin+25_unmasked_criteria"
+        super().__init__(selectors, selection_name = selection_name)
+        
+class Austin25_sample(Multiple_SED_fit_Selector):
+
+    def __init__(
+        self,
+        aper_diam: u.Quantity,
+        SED_fit_label: Union[str, SED_code],
+        simulated: bool = False,
+    ):
+        selectors = [
+            Bluewards_LyLim_Non_Detect_Selector(aper_diam, SED_fit_label, SNR_lim = 2.0, ignore_bands = ["F070W", "F850LP"]), #Ensures no detection shortward of the Lyman limit 
+            Bluewards_Lya_Non_Detect_Selector(aper_diam, SED_fit_label, SNR_lim = 3.0, ignore_bands = ["F070W", "F850LP"]), #Ensures no detection shortward of the Lyman alpha line
+            Redwards_Lya_Detect_Selector(aper_diam, SED_fit_label, SNR_lims = [8.0, 8.0], widebands_only = True, ignore_bands = ["F070W", "F850LP"]), # Requires a strong detection longward of Lyman-alpha, supporting high-redshift identification.
+            Redwards_Lya_Detect_Selector(aper_diam, SED_fit_label, SNR_lims = 3.0, widebands_only = True, ignore_bands = ["F070W", "F850LP"]),
+            Chi_Sq_Lim_Selector(aper_diam, SED_fit_label, chi_sq_lim = 3.0, reduced = True), #cuts objects with poor SED fits (high reduced χ²).
+            Chi_Sq_Diff_Selector(aper_diam, SED_fit_label, chi_sq_diff = 4.0, dz = 0.5), #Ensures the best-fit redshift is significantly better than alternatives.
+            Robust_zPDF_Selector(aper_diam, SED_fit_label, integral_lim = 0.6, dz_over_z = 0.1),
+        ]
+        assert isinstance(simulated, bool), galfind_logger.critical(f"{type(simulated)=}!=bool")
+        if not simulated:
+            selectors.extend([Sextractor_Bands_Radius_Selector(band_names = ["F277W", "F356W", "F444W"], gtr_or_less = "gtr", lim = 45. * u.marcsec)])
+            # add unmasked instrument selections
+            #selectors.extend([Unmasked_Instrument_Selector(instr_name) for instr_name in ["ACS_WFC", "NIRCam"]])
+            selectors.extend([Austin25_unmasked_criteria()])
+        selection_name = "Austin+25"
+        super().__init__(aper_diam, SED_fit_label, selectors, selection_name = selection_name)
+
+    def _assertions(self) -> bool:
+        return True
+
+sample = Austin25_sample
 
 # Define cosmology 
 cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
-
-# Define the sample
-sample = Austin25_sample
 
 # variables for catalogue 
 survey = "JADES-DR3-GS-East"
@@ -215,8 +253,8 @@ plt.errorbar(alba_bb, y_alba, yerr=yerr_alba, fmt='o', color='green', capsize=3)
 plt.xlabel("Balmer Break Strength (mag)")
 plt.ylabel("Number of Galaxies")
 plt.ylabel("Probability Density")
-plt.xlim(-0.6, 2.5)
-plt.ylim(0, 2.5)
+plt.xlim(-0.6, 3.5)
+plt.ylim(0, 4.5)
 plt.legend()
 plt.tight_layout()
 plt.savefig("balmer_break_comparison.png")
