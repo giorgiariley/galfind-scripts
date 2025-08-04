ddc10501dd027ba155b2cad1eda676b510cdb416
Giorgia Riley Summer 2025 <griley@morgan.jb.man.ac.uk>
2025-08-04 14:25:38 +0100

    fixed ratio due to logs

--- Scaling.py
+++ Scaling.py
@@ -1,7 +1,6 @@
-#making a function that scales mass/sfr
+import numpy as np
 from astropy.io import fits
 from astropy.table import Table
-
 
 def load_bagpipes_table(fits_path):
     """Load the Bagpipes output table from a FITS file."""
@@ -9,36 +8,23 @@ def load_bagpipes_table(fits_path):
         table = Table(hdulist[1].data)
     return table
 
-def ratio(filter, table):
-    "takes ratio of auto and aper fluxes for scaling"
-    R_filter =  table[f'Flux_auto_{filter}']/ table[f'Flux_apper_{filter}']
-    if R_filter <1:
-        R_filter = 1
-    elif R_filter > 10:
-        R_filter = 10       
-    
-    return R_filter
+def compute_flux_ratio(filter, table):
+    """Computes the auto/aper flux ratio and clips extreme values."""
+    ratio = table[f'Flux_auto_{filter}'] / table[f'Flux_apper_{filter}']
+    ratio = np.clip(ratio, 1, 10)  # Avoid over- or under-correcting
+    return ratio
 
-
-
-#need to think about log scale and create a new
-def scale_mass_sfr(table, filter):
+def scale_log_mass_sfr(table, filter):
     """
-    Scales the mass and SFR based on the ratio of auto and aperture fluxes.
+    Scales log(stellar mass) and log(SFR) by log10(flux_auto / flux_aperture).
     
-    Parameters:
-    - table: Astropy Table containing the Bagpipes output.
-    - filter: The filter for which to scale the mass and SFR.
-    
-    Returns:
-    - table: Updated Astropy Table with scaled mass and SFR.
+    Updates the table in-place.
     """
-    R_filter = ratio(filter, table)
+    R = compute_flux_ratio(filter, table)
+    logR = np.log10(R)
     
-    # Scale mass and SFR
-    table['stellar_mass_50'] *= R_filter
-    table['sfr_50'] *= R_filter
-    
-    return table    
-        
+    # Assumes original columns are log10 values
+    table['stellar_mass_50'] += logR
+    table['sfr_50'] += logR
 
+    return table
