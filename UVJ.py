709e52c1c75dafd75732abf0295ed4337f4b6bb1
Giorgia Riley Summer 2025 <griley@morgan.jb.man.ac.uk>
2025-08-13 17:04:37 +0100

    fomratting and extreme

--- UVJ.py
+++ UVJ.py
@@ -2,7 +2,12 @@ from astropy.io import fits
 from astropy.table import Table
 import matplotlib.pyplot as plt
 import numpy as np
-
+plt.rcParams.update({
+    "axes.labelsize": 15,   # axis label font
+    "xtick.labelsize": 13,  # tick label font
+    "ytick.labelsize": 13,
+    "legend.fontsize": 13,
+})
 # Load table
 hdulist = fits.open("/raid/scratch/work/Griley/GALFIND_WORK/Catalogues/v13/ACS_WFC+NIRCam/JADES-DR3-GS-East/(0.32)as/JADES-DR3-GS-East_MASTER_Sel-F277W+F356W+F444W_v13.fits")
 table_bagpipes = Table(hdulist[4].data)
@@ -16,14 +21,14 @@ burstiness = table_bagpipes['burstiness_50']
 redshift = table_bagpipes['input_redshift']
 
 # Define mask
-mask = (halpha < 200) & (burstiness < 1)
+mask = (halpha < 25) & (burstiness < 0.5)
 
 # Create plot
 plt.figure(figsize=(8, 6), facecolor='white')
 
 # Plot points 
-plt.scatter(VJ[~mask], UV[~mask], color='tomato', label='Other', alpha=0.6, edgecolor='none')
-plt.scatter(VJ[mask], UV[mask], color='royalblue', label='Hα EW < 200 & burstiness < 1', alpha=0.6, edgecolor='none')
+plt.scatter(VJ[~mask], UV[~mask], color='Royalblue', label='Other', alpha=0.6, edgecolor='none')
+plt.scatter(VJ[mask], UV[mask], color='tomato', label='Hα EW < 25 & burstiness < 0.5', alpha=0.6, edgecolor='none')
 
 
 # UVJ diagram lines
@@ -38,7 +43,6 @@ plt.xlim(-1.5, 2)
 plt.ylim(-1, 3)
 plt.xlabel("V–J (AB mag)")
 plt.ylabel("U–V (AB mag)")
-plt.title("UVJ Diagram for JADES-DR3_GS-East")
 plt.legend()
 plt.grid(True)
 plt.tight_layout()
